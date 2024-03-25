9ce30777420c89aaf33be9547e67a73336f727b3
rianaw04 <warnerriana1@gmail.com>
2024-03-25 11:39:46 -0400

    week four

---
+++ scripts/w04-task.js
@@ -0,0 +1,70 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Riana Warner",
+    photo: "images/riana.jpg",
+    favoriteFoods: ["Pizza", "Sushi", "Burgers", "Tacos", "Pasta"],
+    hobbies: ["Reading", "Hiking", "Swimming", "Coding"],
+    placesLived: []
+    
+}
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+document.querySelector('#name').textContent = myProfile.name;
+
+/* Photo with attributes */
+document.getElementById('photo').src = myProfile.photo;
+
+document.getElementById('photo').alt = myProfile.name;
+
+
+/* Favorite Foods List*/
+
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(li);
+  });
+  
+
+/* Hobbies List */
+myProfile.hobbies.forEach(hobby => {
+    let li = document.createElement('li');
+    li.textContent = hobby;
+    document.querySelector('#hobbies').appendChild(li);
+  });
+  
+
+/* Places Lived DataList */
+
+myProfile.placesLived.push({
+    place: "Antigua",
+    length: "10+ years"
+});
+
+
+myProfile.placesLived.forEach(place => {
+    let dt = document.createElement('dt');
+    dt.textContent = place.place;
+    let dd = document.createElement('dd');
+    dd.textContent = place.length;
+    
+   
+    let placesLivedDL = document.querySelector('#places-lived');
+    placesLivedDL.appendChild(dt);
+    placesLivedDL.appendChild(dd);
+});
+
+
+
+
